e892b50bae6991b20a1bf7f9364c0d72ecb69318
SaadHusayn <123408017+SaadHusayn@users.noreply.github.com>
2024-09-29 11:55:22 +0500

    Time: 12 ms (88.23%), Space: 27.3 MB (66.58%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    void revStringRecursion(vector<char> &s, int start, int end){
+    void revStringRecursion(vector<char> &s, int start){
+        auto end = s.size() - start - 1;
         if(start>=end) return;
         
-        swap(s[start++], s[end--]);
-        revStringRecursion(s, start, end);
+        swap(s[start++], s[end]);
+        revStringRecursion(s, start);
     }
     void reverseString(vector<char>& s) {
-        revStringRecursion(s, 0, s.size()-1);
+        revStringRecursion(s, 0);
     }
 };
