f52ccbe7c1faf2041cd7fc798f008e1a4d66e438
SaadHusayn <123408017+SaadHusayn@users.noreply.github.com>
2024-11-13 23:14:05 +0500

    Time: 0 ms (100.00%), Space: 16.7 MB (53.23%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -13,6 +13,15 @@ public:
         int n = cost.size();
         cost.push_back(0);
         vector<int> dp(n+1, -1);
-        return solve(cost, n, dp);
+        // return solve(cost, n, dp);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        
+        return dp[n];
     }
 };
